28e2c34280ddbe80f131466e4c1b435648e12ba7
Ghost <koffijude33@gmail.com>
2025-07-29 00:43:08 +0000

    routers

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('auth')->group(function () {
+    require __DIR__ . '/routers/auth.php';
+});
 Route::middleware('auth:api')->group(function () {
-    Route::prefix('auth')->group(function () {
-        require __DIR__ . '/routers/auth.php';
-    });
 
     Route::prefix('app')->group(function () {
         require __DIR__ . '/routers/app.php';
